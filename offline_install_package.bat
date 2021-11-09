e9987fe62e50e8cc5512573efe8ccf81b2b33b59
TsungJu <e285171@gmail.com>
2021-11-09 11:41:15 +0800

    Add Feature:Add embed python 3.9.1(Included pip) , offline install bat file and offline install package

---
+++ offline_install_package.bat
@@ -0,0 +1,10 @@
+@echo off
+".\python.exe" -m pip install --no-index --find-links PIP_Download_Package PIP_Download_Package\matplotlib-3.4.2-cp39-cp39-win_amd64.whl
+".\python.exe" -m pip install --no-index --find-links PIP_Download_Package PIP_Download_Package\numpy-1.21.3-cp39-cp39-win_amd64.whl
+".\python.exe" -m pip install --no-index --find-links PIP_Download_Package PIP_Download_Package\pandas-1.3.0-cp39-cp39-win_amd64.whl
+".\python.exe" -m pip install --no-index --find-links PIP_Download_Package PIP_Download_Package\scikit_learn-1.0.1-cp39-cp39-win_amd64.whl
+".\python.exe" -m pip install --no-index --find-links PIP_Download_Package PIP_Download_Package\scipy-1.7.1-cp39-cp39-win_amd64.whl
+".\python.exe" -m pip install --no-index --find-links PIP_Download_Package PIP_Download_Package\six-1.16.0-py2.py3-none-any.whl
+".\python.exe" -m pip install --no-index --find-links PIP_Download_Package PIP_Download_Package\sklearn-0.0.tar.gz
+".\python.exe" -m pip install --no-index --find-links PIP_Download_Package PIP_Download_Package\xgboost-0.90-py2.py3-none-win_amd64.whl
+pause
